1393a5e761e403814e0f0fe8d9586b088ae779d3
AbdullahAbdelaziz122 <abdullah.abdelaziz15@gmail.com>
2025-04-20 13:16:06 +0200

    timer project

--- Timer/timer.vhd
+++ Timer/timer.vhd
@@ -3,20 +3,57 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity timer is
+generic(clockFrequency : integer);
 port(
-	clk    : in std_logic;
-	nRst   : in std_logic -- Negative Reset
+	clk     : in std_logic;
+	nRst    : in std_logic; -- Negative Reset
+	seconds : inout integer;
+	minutes : inout integer;
+	hours   : inout integer
 );
 end entity timer;
 architecture rtl of timer is
 
+	signal ticks : integer;
+	
 begin
 	process (clk) is
 	begin
 		
 		if rising_edge(clk) then
 			if nRst = '0' then
+				ticks   <= 0;
+				seconds <= 0;
+				minutes <= 0;
+				hours   <= 0;
 			else
+				
+				-- True once every second
+				if ticks = clockFrequency - 1 then
+					ticks <= 0;
+					
+					if seconds = 59 then
+						seconds <= 0;
+						
+						-- True once every minute
+						if minutes = 59 then
+							minutes <= 0;
+							
+							-- True once every hour
+							if hours = 23 then
+								hours <= 0;
+							else
+								hours <= hours + 1;
+							end if;
+						else
+							minutes <= minutes + 1;
+						end if;
+					else
+						seconds <= seconds + 1;
+					end if;
+				else
+					ticks <= ticks +1;
+				end if;
 			end if;
 		end if;
 	end process;
--- Timer/timerTb.vhd
+++ Timer/timerTb.vhd
@@ -2,19 +2,34 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity clockedProcessTb is
-end entity clockedProcessTb;
+entity timerTb is
+end entity timerTb;
 
-architecture sim of clockedProcessTb is
-	constant clockFrequency : integer := 100e6; -- 100^6 [100 MHZ]
+architecture sim of timerTb is
+	--constant clockFrequency : integer := 100e6; -- 100^6 [100 MHZ]
+	
+	-- we are slowing down the clock to speed simulation time
+	constant clockFrequency : integer := 10; -- 10 Hz 
 	constant clockPeriod : time := 1000 ms / clockFrequency;
 	
-	signal clk 	  : std_logic := '1';
-	signal nRst	  : std_logic := '0';
+	signal clk 	   : std_logic := '1';
+	signal nRst	   : std_logic := '0';
+	signal seconds : integer;
+	signal minutes : integer;
+	signal hours   : integer;
+	 
 begin
 	
 	-- The Device Under Test (DUT)
-	
+	i_Timer : entity work.timer(rtl)
+	generic map (clockFrequency => clockFrequency)
+	port map (
+		clk     => clk,
+		nRst    => nRst,
+		seconds => seconds,
+		minutes => minutes,
+		hours   => hours
+	);
 
 	
 	-- process for generating the clock
@@ -23,7 +38,11 @@ begin
 	-- Testbench sequence 
 	process is
 	begin
+		wait until rising_edge(clk);
+		wait until rising_edge(clk);
 		
+		-- Take the DUT out of reset
+		nRst <= '1';
 		wait;
 	end process;
 	
